e61ceccf1ec94999304e5c67a74f55bdd76bbf27
Adam Zadrożny <zadroznyadam@pm.me>
2024-05-29 11:27:46 +0200

    Day 14: switch from `grid` to `ndarray` (1.78x speedup)
    Apart from speed benefits, ndarray's slicing capabilites allow
    us to implement a general `roll` function simplifying the code.

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,19 +1,20 @@
-use aoc2023::read_grid;
-use simple_grid::Grid;
+use aoc2023::read_2d_array;
+use ndarray::{prelude::*, Zip};
 
 fn main() {
     let input = std::fs::read_to_string("inputs/day14").unwrap();
-    let grid = read_grid(&input);
-    println!("Part 1: {}", load_of(&roll_north(grid.clone())));
-    println!("Part 2: {}", load_after(1_000_000_000, grid));
+    let grid = read_2d_array(&input);
+    println!("Part 1: {}", solve_part1(grid.clone()));
+    println!("Part 2: {}", solve_part2(grid));
 }
 
-fn cycle(grid: Grid<u8>) -> Grid<u8> {
-    roll_east(roll_south(roll_west(roll_north(grid))))
+fn solve_part1(mut arr: Array2<u8>) -> usize {
+    roll(arr.view_mut());
+    load_of(arr.view())
 }
 
-fn load_after(time: usize, grid: Grid<u8>) -> usize {
-    let mut iters = vec![grid];
+fn solve_part2(arr: Array2<u8>) -> usize {
+    let mut iters = vec![arr];
     let start = loop {
         let g = cycle(iters.last().unwrap().clone());
         if let Some(i) = iters.iter().position(|x| *x == g) {
@@ -22,104 +23,79 @@ fn load_after(time: usize, grid: Grid<u8>) -> usize {
         iters.push(g);
     };
     let period = iters.len() - start;
-    load_of(&iters[start + (time - start) % period])
+    load_of(iters[start + (1_000_000_000 - start) % period].view())
 }
 
-fn load_of(grid: &Grid<u8>) -> usize {
-    let h = grid.height();
-    grid.cells_with_indices_iter()
-        .filter_map(|(idx, c)| (*c == b'O').then_some(idx.row()))
-        .fold(0, |acc, row| acc + h - row)
+fn cycle(grid: Array2<u8>) -> Array2<u8> {
+    roll_east(roll_south(roll_west(roll_north(grid))))
 }
 
-fn roll_north(mut grid: Grid<u8>) -> Grid<u8> {
-    for col in grid.columns() {
-        let mut idx = 0;
-        for row in grid.rows() {
-            match grid[(col, row)] {
+fn load_of(view: ArrayView2<u8>) -> usize {
+    let h = view.ncols();
+    Zip::indexed(view).fold(
+        0,
+        |acc, (y, _), &elem| if elem == b'O' { acc + h - y } else { acc },
+    )
+}
+
+/// If passed an `array.view_mut()` this rolls north.
+/// To roll in other direction, reverse one of the axes and/or swap the axes
+fn roll(mut view: ArrayViewMut2<u8>) {
+    for x in 0..view.ncols() {
+        let mut i = 0;
+        for y in 0..view.nrows() {
+            match view[[y, x]] {
                 b'O' => {
-                    grid[(col, row)] = b'.';
-                    grid[(col, idx)] = b'O';
-                    idx += 1;
+                    view.swap([y, x], [i, x]);
+                    i += 1;
                 }
-                b'#' => idx = row + 1,
+                b'#' => i = y + 1,
                 _ => {}
             }
         }
     }
-    grid
 }
 
-fn roll_south(mut grid: Grid<u8>) -> Grid<u8> {
-    for col in grid.columns() {
-        let mut idx = grid.height();
-        for row in grid.rows().rev() {
-            match grid[(col, row)] {
-                b'O' => {
-                    idx -= 1;
-                    grid[(col, row)] = b'.';
-                    grid[(col, idx)] = b'O';
-                }
-                b'#' => idx = row,
-                _ => {}
-            }
-        }
-    }
-    grid
+#[inline]
+fn roll_north(mut arr: Array2<u8>) -> Array2<u8> {
+    roll(arr.slice_mut(s![.., ..]));
+    arr
 }
 
-fn roll_east(mut grid: Grid<u8>) -> Grid<u8> {
-    for row in grid.rows() {
-        let mut idx = grid.width();
-        for col in grid.columns().rev() {
-            match grid[(col, row)] {
-                b'O' => {
-                    idx -= 1;
-                    grid[(col, row)] = b'.';
-                    grid[(idx, row)] = b'O';
-                }
-                b'#' => idx = col,
-                _ => {}
-            }
-        }
-    }
-    grid
+#[inline]
+fn roll_south(mut arr: Array2<u8>) -> Array2<u8> {
+    roll(arr.slice_mut(s![..;-1, ..]));
+    arr
 }
 
-fn roll_west(mut grid: Grid<u8>) -> Grid<u8> {
-    for row in grid.rows() {
-        let mut idx = 0;
-        for col in grid.columns() {
-            match grid[(col, row)] {
-                b'O' => {
-                    grid[(col, row)] = b'.';
-                    grid[(idx, row)] = b'O';
-                    idx += 1;
-                }
-                b'#' => idx = col + 1,
-                _ => {}
-            }
-        }
-    }
-    grid
+#[inline]
+fn roll_east(mut arr: Array2<u8>) -> Array2<u8> {
+    roll(arr.slice_mut(s![.., ..;-1]).reversed_axes());
+    arr
+}
+
+#[inline]
+fn roll_west(mut arr: Array2<u8>) -> Array2<u8> {
+    roll(arr.slice_mut(s![.., ..]).reversed_axes());
+    arr
 }
 
 #[cfg(test)]
 mod tests {
-    use aoc2023::read_grid;
+    use aoc2023::read_2d_array;
 
     use super::*;
 
     #[test]
     fn can_solve_part1() {
-        let grid = read_grid(SAMPLE1);
-        assert_eq!(load_of(&roll_north(grid)), 136);
+        let grid = read_2d_array(SAMPLE1);
+        assert_eq!(solve_part1(grid), 136);
     }
 
     #[test]
     fn can_solve_part2() {
-        let grid = read_grid(SAMPLE1);
-        assert_eq!(load_after(1_000_000_000, grid), 64);
+        let grid = read_2d_array(SAMPLE1);
+        assert_eq!(solve_part2(grid), 64);
     }
 
     const SAMPLE1: &str = "
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use ndarray::prelude::*;
 use simple_grid::Grid;
 
 /// Read a rectangular matrix of data from a string, where each byte encodes a single value
@@ -26,4 +27,31 @@ pub fn read_grid_with<T>(s: &str, transform: impl Fn(&u8) -> T) -> Grid<T> {
         .map(transform)
         .collect();
     Grid::new(w, h, data)
+}
+
+/// Read a rectangular matrix of data from a string, where each byte encodes a single value
+/// and rows are separated by newlines. Extra whitespace is allowed both around the grid and between the rows.
+/// # Panics
+/// * if `s` doesn't contain at least one row
+/// * if rows have different lengths
+pub fn read_2d_array(s: &str) -> Array2<u8> {
+    read_2d_array_with(s, |b| *b)
+}
+
+/// Read a rectangular matrix of data from a string, where each byte encodes a single value
+/// with a user supplied translation function, and rows are separated by newlines.
+/// Extra whitespace is allowed both around the ndarray and between the rows.
+/// # Panics
+/// * if `s` doesn't contain at least one row
+/// * if rows have different lengths
+pub fn read_2d_array_with<T>(s: &str, transform: impl Fn(&u8) -> T) -> Array2<T> {
+    let lines: Vec<_> = s.trim().lines().collect();
+    let h = lines.len();
+    let w = lines[0].len();
+    let data = lines
+        .into_iter()
+        .flat_map(|s| s.trim().as_bytes())
+        .map(transform)
+        .collect();
+    Array2::from_shape_vec((h, w), data).unwrap()
 }
